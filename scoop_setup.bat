e688f51954b6bd74185437df926596a4943b76bb
Richan-Oreta <richanmoreta@gmail.com>
2022-06-26 13:26:28 +0800

    deleted duplicate scoop setup

--- scoop_setup.bat
+++
@@ -1,1 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser && Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') && scoop bucket add extras && scoop bucket add nonportable
